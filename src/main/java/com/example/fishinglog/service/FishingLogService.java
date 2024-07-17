b54a3bc655086f466ec8e2c1644b30fc77ee2b13
Danny Cao <dannycao1997@yahoo.com>
2024-07-17 01:51:42 -0400

    implemented business logic for service class

--- src/main/java/com/example/fishinglog/service/FishingLogService.java
+++ src/main/java/com/example/fishinglog/service/FishingLogService.java
@@ -1,4 +1,43 @@
 package com.example.fishinglog.service;
 
+import com.example.fishinglog.entity.FishingLog;
+import com.example.fishinglog.repository.FishingLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FishingLogService {
+
+    @Autowired
+    private FishingLogRepository fishingLogRepository;
+
+    public List<FishingLog> getAllLogs(){
+        return fishingLogRepository.findAll();
+    }
+
+    public Optional<FishingLog> getLogById(Long id){
+        return fishingLogRepository.findById(id);
+    }
+
+    public FishingLog createLog(FishingLog fishingLog){
+        return fishingLogRepository.save(fishingLog);
+    }
+
+    public FishingLog updateLog(Long id, FishingLog fishingLog) {
+        Optional<FishingLog> existingLog = fishingLogRepository.findById(id);
+        if (existingLog.isPresent()) {
+            FishingLog logToUpdate = existingLog.get();
+
+            logToUpdate.setFishSpecies(fishingLog.getFishSpecies());
+            logToUpdate.setDate(fishingLog.getDate());
+            logToUpdate.setLocation(fishingLog.getLocation());
+            logToUpdate.setBait(fishingLog.getBait());
+
+            return fishingLogRepository.save(logToUpdate);
+        }
+        return null;
+    }
 }
