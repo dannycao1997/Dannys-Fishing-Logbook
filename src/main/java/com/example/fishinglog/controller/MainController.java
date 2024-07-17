e15d20af61e40598c3c6211a51d4b9f17f879656
Danny Cao <dannycao1997@yahoo.com>
2024-07-17 11:49:32 -0400

    Initial commit

---
+++ src/main/java/com/example/fishinglog/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.fishinglog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = "/**/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/";
+    }
+}
