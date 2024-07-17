5d33c397655c59ba482eeee0816e7a4d22383442
Danny Cao <dannycao1997@yahoo.com>
2024-07-17 02:06:20 -0400

    complete controller class and testing out api end points

--- src/main/java/com/example/fishinglog/controller/FishingLogController.java
+++ src/main/java/com/example/fishinglog/controller/FishingLogController.java
@@ -1,4 +1,47 @@
 package com.example.fishinglog.controller;
 
+import com.example.fishinglog.entity.FishingLog;
+import com.example.fishinglog.service.FishingLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/logs")
 public class FishingLogController {
+
+    @Autowired
+    private FishingLogService fishingLogService;
+
+    @GetMapping
+    public List<FishingLog> getAllLogs(){
+        return fishingLogService.getAllLogs();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FishingLog> getLogById(@PathVariable Long id) {
+        Optional<FishingLog> log = fishingLogService.getLogById(id);
+        return log.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public FishingLog createLog(@RequestBody FishingLog fishingLog) {
+        return fishingLogService.createLog(fishingLog);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FishingLog> updateLog (@PathVariable Long id, @RequestBody FishingLog fishingLog) {
+        FishingLog updateLog = fishingLogService.updateLog(id, fishingLog);
+        return updateLog != null ? ResponseEntity.ok(updateLog) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLog(@PathVariable Long id){
+        fishingLogService.deleteLog(id);
+        return ResponseEntity.noContent().build();
+
+    }
 }
--- src/main/java/com/example/fishinglog/service/FishingLogService.java
+++ src/main/java/com/example/fishinglog/service/FishingLogService.java
@@ -40,4 +40,8 @@ public class FishingLogService {
         }
         return null;
     }
+
+    public void deleteLog(Long id){
+        fishingLogRepository.deleteById(id);
+    }
 }
