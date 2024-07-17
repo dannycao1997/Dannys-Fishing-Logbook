7bd05e4f4ea3afff73e1529924700dfe44500b1d
Danny Cao <dannycao1997@yahoo.com>
2024-07-17 01:10:11 -0400

    fishinglogrepository interface  extending jparepository

--- src/main/java/com/example/fishinglog/repository/FishingLogRepository.java
+++ src/main/java/com/example/fishinglog/repository/FishingLogRepository.java
@@ -1,4 +1,10 @@
 package com.example.fishinglog.repository;
 
-public interface FishingLogRepository {
+import com.example.fishinglog.entity.FishingLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FishingLogRepository extends JpaRepository<FishingLog, Long> {
+
 }
