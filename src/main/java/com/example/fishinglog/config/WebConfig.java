c11fc9c22e3012b30f04beb65773d6d70f434547
Danny Cao <dannycao1997@yahoo.com>
2024-08-14 00:53:58 -0400

    Update CORS settings

--- src/main/java/com/example/fishinglog/config/WebConfig.java
+++ src/main/java/com/example/fishinglog/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://frontend-fishing-5f99ebe2a958.herokuapp.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
